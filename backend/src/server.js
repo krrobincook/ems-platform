3133aafa5995f512c477c303e05d5a6408da1123
krrobincook <kumarrobincook@gmail.com>
2026-06-03 12:18:57 +0530

    Register Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { connectDB } from './config/database.js';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config({path: '../.env'});
 await connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
